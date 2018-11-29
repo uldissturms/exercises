5e539d6ecee3fab39c3ed6658a76f1c9e687b915
Uldis Sturms <uldis.sturms@fil.com>
2018-11-29 14:05:46 +0000

    feat(leetcode-37): brute force sudoku solver

--- leetcode/37-sudoku-solver.test.js
+++ leetcode/37-sudoku-solver.test.js
@@ -1,61 +1,69 @@
 // [https://leetcode.com/problems/sudoku-solver]
 
 const test = require('ava')
+const {isUndefined} = require('../helpers')
 
-test.skip('solves a sudoku', t => {
-  const input = [
-    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-    ['.', '.', '.', '.', '8', '.', '.', '7', '9']
-  ]
+const createInput = () => [
+  ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+  ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+  ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+  ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+  ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+  ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+  ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+  ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+  ['.', '.', '.', '.', '8', '.', '.', '7', '9']
+]
 
+// example of solved sudoku
+// [
+//   [ '5', '3', '4', '6', '7', '8', '9', '1', '2' ],
+//   [ '6', '7', '2', '1', '9', '5', '3', '4', '8' ],
+//   [ '1', '9', '8', '3', '4', '2', '5', '6', '7' ],
+//   [ '8', '5', '9', '7', '6', '1', '4', '2', '3' ],
+//   [ '4', '2', '6', '8', '5', '3', '7', '9', '1' ],
+//   [ '7', '1', '3', '9', '2', '4', '8', '5', '6' ],
+//   [ '9', '6', '1', '5', '3', '7', '2', '8', '4' ],
+//   [ '2', '8', '7', '4', '1', '9', '6', '3', '5' ],
+//   [ '3', '4', '5', '2', '8', '6', '1', '7', '9' ]
+// ]
+
+test('returns valid characters', t => {
+  const input = createInput()
+  t.deepEqual(
+    getValidChars(0, 0, input),
+    new Set(['1', '2'])
+  )
+  t.deepEqual(
+    getValidChars(0, 1, input),
+    new Set(['1', '2', '4'])
+  )
+})
+
+test('solves a sudoku', t => {
+  const input = createInput()
   const solved = solveSudoku(input)
   t.true(solved)
 })
 
 const EMPTY = '.'
-const SEGMENT_SIZE = 3
-const ALL_CHARS = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
-const isEmpty = arr => arr.length === 0
+const ALL_CHARS_ARR = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+const ALL_CHARS = new Set(ALL_CHARS_ARR)
+const SEGMENT_SIZE = Math.sqrt(ALL_CHARS_ARR.length)
 
 const solveSudoku = arr => solve(0, 0, arr)
 
-const outOfScope = (row, col, arr) =>
-  row >= arr.length || col >= arr.length
-
 const solve = (row, col, arr) => {
-  if (row === 4) {
-    console.log(arr)
-    return true
-  }
-
-  console.log('row:', row, 'col:', col)
-  if (outOfScope(row, col, arr)) {
-    return true
-  }
-
   const next = getNext(row, col, arr)
 
   if (arr[row][col] !== EMPTY) {
-    return solve(next.row, next.col, arr)
+    return isUndefined(next) || solve(next.row, next.col, arr)
   }
 
-  const validMoves = getValidChars(row, col, arr)
-  if (isEmpty(validMoves)) {
-    return false
-  }
-  console.log('valid moves:', validMoves)
-  for (const move of validMoves) {
-    console.log('Setting:', row, col, 'to:', move)
-    arr[row][col] = move
-    const nextValid = solve(next.row, next.col, arr)
-    if (nextValid) {
+  for (const char of getValidChars(row, col, arr)) {
+    arr[row][col] = char
+    const valid = isUndefined(next) || solve(next.row, next.col, arr)
+    if (valid) {
       return true
     }
     arr[row][col] = EMPTY
@@ -77,30 +85,33 @@ const getNext = (row, col, arr) => {
 }
 
 const getValidChars = (row, col, arr) => {
-  const taken = [
+  const taken = new Set([
     ...arr[row],
     ...arr.map(x => x[col]),
     ...charsInSegmentFor(row, col, arr)
-  ]
+  ])
   const valid = new Set(ALL_CHARS.values())
   for (const c of taken) {
     valid.delete(c)
   }
-  return valid.values()
+  return valid
 }
 
 const segmentFor = i =>
-  i % SEGMENT_SIZE
+  Math.floor(i / SEGMENT_SIZE)
+
+const offsetForSegment = i =>
+  segmentFor(i) * SEGMENT_SIZE
 
 const charsInSegmentFor = (row, col, arr) => {
-  const rowOffset = segmentFor(row) % SEGMENT_SIZE
-  const colOffset = segmentFor(col) % SEGMENT_SIZE
+  const rowOffset = offsetForSegment(row)
+  const colOffset = offsetForSegment(col)
 
   const chars = []
 
   for (let i = 0; i < SEGMENT_SIZE; i++) {
     for (let j = 0; j < SEGMENT_SIZE; j++) {
-      chars.push(arr[rowOffset + i][colOffset + i])
+      chars.push(arr[rowOffset + i][colOffset + j])
     }
   }
 
