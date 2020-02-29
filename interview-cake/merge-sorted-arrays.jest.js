adb95a3ff741181b447a56e22abdcf2dcb0fc3e3
Uldis Sturms <uldis.sturms@gmail.com>
2020-02-29 11:06:32 +0000

    feat(interview-cake): merge arrays - support mergin n arrays

--- interview-cake/merge-sorted-arrays.jest.js
+++ interview-cake/merge-sorted-arrays.jest.js
@@ -32,6 +32,10 @@ test('different size of arrays', () => {
   expect(merge([2, 4], [1, 3, 5])).toEqual([1, 2, 3, 4, 5])
 })
 
+test('merge multiple arrays', () => {
+  expect(merge([1, 4], [2, 5, 7], [3, 6])).toEqual([1, 2, 3, 4, 5, 6, 7])
+})
+
 /*
 resoning:
 keep 2 pointers to always insert the smallest item from both arrays
@@ -46,23 +50,30 @@ steps:
     increment the smaller pointer by one
 */
 
+const sum = (x, y) => x + y
+
 const isUndefined = x => typeof x === 'undefined'
-const merge = (xs, ys) => {
-  const dst = new Array(xs.length + ys.length)
-  let xIdx = 0
-  let yIdx = 0
-  while (xIdx < xs.length || yIdx < ys.length) {
-    const idx = xIdx + yIdx
-    const x = xs[xIdx]
-    const y = ys[yIdx]
-    let val
-    if (isUndefined(x) || x > y) { // only ys left, or y is less than x
-      val = y
-      yIdx++
-    } else { // y is undefined, x is less or equal to y
-      val = x
-      xIdx++
+
+const getMin = (arrs, ptrs) => {
+  let minPtrIdx
+  let minVal
+  ptrs.forEach((p, pIdx) => {
+    const curr = arrs[pIdx][p]
+    if (!isUndefined(curr) && (curr < minVal || isUndefined(minPtrIdx))) {
+      minVal = curr
+      minPtrIdx = pIdx
     }
+  })
+  return [minPtrIdx, minVal]
+}
+
+const merge = (...arrs) => {
+  const dst = new Array(arrs.map(x => x.length).reduce(sum, 0))
+  const ptrs = new Array(arrs.length).fill(0)
+  while (ptrs.some((x, idx) => x < arrs[idx].length)) {
+    const idx = ptrs.reduce(sum, 0)
+    const [ptrIdx, val] = getMin(arrs, ptrs)
+    ptrs[ptrIdx] = ptrs[ptrIdx] + 1
     dst[idx] = val
   }
   return dst
