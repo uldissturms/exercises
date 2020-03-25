fd64211eb5fc8bdcc669e75e9b6eb8fd62679291
Uldis Sturms <uldis.sturms@gmail.com>
2020-03-25 11:11:40 +0000

    feat(codility-31): count non-divisible - alternative solution with O(n * logn) time complexity

--- codility/31-count-non-divisible.jest.js
+++ codility/31-count-non-divisible.jest.js
@@ -1,12 +1,16 @@
 test('solve', () => {
-  expect(solve([3, 1, 2, 3, 6])).toEqual([2, 4, 3, 2, 0])
   expect(solve([1])).toEqual([0])
   expect(solve([1, 2])).toEqual([1, 0])
+  expect(solve([3, 4])).toEqual([1, 1])
+  expect(solve([2])).toEqual([0])
   expect(solve([1, 2, 3])).toEqual([2, 1, 1])
+
+  // example
+  expect(solve([3, 1, 2, 3, 6])).toEqual([2, 4, 3, 2, 0])
 })
 
 test('solve - large', () => {
-  const size = 20000
+  const size = 50000
   const res = solve(new Array(size).fill(0).map((_, i) => i))
   expect(res).toHaveLength(size)
 })
@@ -16,37 +20,58 @@ const solveBF = (xs) => {
   return xs.map((x, i) => xs.filter((y, j) => i !== j && x % y !== 0).length)
 }
 
-// O(n * sqrt(n))
-const solveSR = (xs) => {
-  const len = xs.length
-  const max = len * 2
-  const counts = new Array(max).fill(0)
+const countOccurrences = (xs) => {
+  const counts = new Array((xs.length * 2) + 1).fill(0)
 
   for (const x of xs) {
     counts[x] = counts[x] + 1
   }
 
-  const countFromFactors = (x) => {
-    let count = 0
-    let root = Math.floor(Math.sqrt(x))
-
-    if (Math.pow(root, 2) === x) {
-      count++
-    } else {
-      root++
-    }
-
-    for (i = 1; i < root; i++) {
-      if (x % i === 0) {
-        count += counts[i]
-        count += counts[x / i]
-      }
-    }
-
-    return count
-  }
-
-  return xs.map(x => len - countFromFactors(x))
+  return counts
 }
 
-const solve = solveSR
+const countFromFactors = (x, counts) => {
+  let count = 0
+  let root = Math.floor(Math.sqrt(x))
+
+  if (Math.pow(root, 2) === x) {
+    count += counts[root]
+  } else {
+    root++
+  }
+
+  for (i = 1; i < root; i++) {
+    if (x % i === 0) {
+      count += counts[i]
+      count += counts[x / i]
+    }
+  }
+
+  return count
+}
+
+// O(n * sqrt(n))
+const solveSR = (xs) => {
+  const counts = countOccurrences(xs)
+  return xs.map(x => xs.length - countFromFactors(x, counts))
+}
+
+// O(n * log(n))
+const solveLg = (xs) => {
+  const counts = countOccurrences(xs)
+  const max = xs.length * 2
+  const sieve = new Array(max + 1).fill(0)
+
+  for (let i = 1; i <= max; i++) {
+    const n = counts[i]
+    if (n !== 0) {
+      for (j = i; j <= max; j += i) {
+        sieve[j] -= n
+      }
+    }
+  }
+
+  return xs.map(x => xs.length + sieve[x])
+}
+
+const solve = solveLg
