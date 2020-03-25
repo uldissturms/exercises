cbcea07607634f2483a19863a66c3c47a86b8a25
Uldis Sturms <uldis.sturms@gmail.com>
2020-03-25 14:07:23 +0000

    chore(leetcode-204): another way to calculate sum of primes less than n

--- leetcode/204-count-primes.jest.js
+++ leetcode/204-count-primes.jest.js
@@ -4,6 +4,7 @@ test('solve', () => {
 
 const solve = n => sieve(n).filter(x => x < n).length
 
+// standard sieve + map & filter
 const sieve = x => {
   const root = Math.floor(Math.sqrt(x))
   const flags = new Array(x + 1).fill(true)
@@ -23,4 +24,22 @@ const sieve = x => {
     .map((f, fIdx) => [f, fIdx])
     .filter(([f]) => f === true)
     .map(([f, fIdx]) => fIdx)
+}
+
+// another way to solve using a counter in the loop and iterating over all n integers
+const solveC = x => {
+  const root = Math.floor(Math.sqrt(x))
+  const flags = new Uint8Array(x + 1)
+  let count = 0
+
+  for (let i = 2; i <= x; i++) {
+    if (!flags[i]) {
+      count++
+      for (let j = i * i; j <= x; j = j + i) {
+        flags[j] = true
+      }
+    }
+  }
+
+  return count
 }
