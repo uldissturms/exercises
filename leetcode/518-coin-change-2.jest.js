ca859aa1cdc680a96d33c0c80b0717aa19560394
Uldis Sturms <uldis.sturms@gmail.com>
2020-04-02 17:10:08 +0100

    feat(leetcode-518): ways of coin change

--- leetcode/518-coin-change-2.jest.js
+++ leetcode/518-coin-change-2.jest.js
@@ -1,10 +1,12 @@
 // [https://leetcode.com/problems/coin-change-2]
+// [https://codility.com/media/train/15-DynamicProgramming.pdf]
 
-test('solve - recursively', () => {
+test('solve - recursively - partial solution', () => {
   expect(solveR([1, 2, 5], 5)).toEqual(4)
+  expect(solveR([1, 2, 5], 6)).toEqual(5)
   expect(solveR([1, 3, 4], 5)).toEqual(3)
   expect(solveR([2], 3)).toEqual(0)
-  expect(solveR([2], 3)).toEqual(0)
+  expect(solveI([3], 2)).toEqual(0)
   expect(solveR([10], 10)).toEqual(1)
 
   /*
@@ -15,37 +17,25 @@ test('solve - recursively', () => {
 
 test('solve - iteratively', () => {
   expect(solveI([1, 2, 5], 5)).toEqual(4)
+  expect(solveI([1, 2, 5], 6)).toEqual(5)
   expect(solveI([2], 3)).toEqual(0)
-  expect(solveI([2], 3)).toEqual(0)
+  expect(solveI([3], 2)).toEqual(0)
   expect(solveI([10], 10)).toEqual(1)
 
-  /*
-  // medium input
-  expect(solveI([1, 2, 5], 100)).toEqual(12701)
-  */
+  expect(solveI([1, 2, 5], 500)).toEqual(12701)
 })
 
 /*
 
+manually:
+
 0 1 2 3 4 5 6
 0 0 0 0 0 0 0
 1 1 1 1 1 1 1
-2 1 2 2 3 3 4 // 6 * 1s, 3 * 2s, 2 * 2s + 2 * 1s, 1 * 2s + 4 * 1s
-5 1 2 2 3 4
-
-
-0: Set()
-1: Set(1)
-2: Set(2, 1)
-3: Set(3, 2)
-4: Set(4, 3, 2)
-5: Set(5)
-
-c: 2
+2 1 2 2 3 3 4
+5 1 2 2 3 4 5
 
 */
-
-const debug = true
 
 const solveR = (cs, x) => {
   const s = new Set()
@@ -69,27 +59,15 @@ const solveR = (cs, x) => {
   return s.size
 }
 
-const combinationsOf = (c, ps, s) => {
-  if (!ps) {
-    return
-  }
-
-  for (const p of [...ps]) {
-    s.add(p + 1)
-  }
-
-  return s
-}
-
 const solveI = (cs, x) => {
-  const dp = new Array(x + 1).fill(undefined).map(x => new Set())
-  dp[0] = new Set([0])
+  const dp = new Array(x + 1).fill(0)
+  dp[0] = 1
 
   for (const c of cs) {
     for (let i = c; i <= x; i++) {
-      dp[i] = combinationsOf(c, dp[i - c], dp[i])
+      dp[i] = dp[i] + dp[i - c]
     }
   }
 
-  return dp[x].size
+  return dp[x]
 }
