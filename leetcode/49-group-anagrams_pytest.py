613258ffc72147b0b02a8cf913ae3dc489f23e8b
Uldis Sturms <uldis.sturms@gmail.com>
2020-04-06 11:55:58 +0100

    feat(leetcode-49): group anagrams

--- leetcode/49-group-anagrams_pytest.py
+++ leetcode/49-group-anagrams_pytest.py
@@ -1,7 +1,38 @@
 # [https://leetcode.com/problems/group-anagrams]
 
-def inc(x):
-  return x + 1
 
-def test_answer():
-    assert inc(3) == 4
+class Solution:
+    def groupAnagrams(self, strs):
+        return solve(strs)
+
+
+def solve(xs):
+    d = dict()
+    for x in xs:
+        s = ''.join(sorted(x))
+        if (s in d):
+            d[s].append(x)
+        else:
+            d[s] = [x]
+
+    return [v for v in d.values()]
+
+
+def test_solve_example():
+    assert solve(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']) == [
+        ['eat', 'tea', 'ate'],
+        ['tan', 'nat'],
+        ['bat']
+    ]
+
+    def test_solve_single():
+        assert solve(['eat']) == [['eat']]
+
+    def test_solve_two():
+        assert solve(['eat', 'ate']) == [['eat', 'ate']]
+
+    def test_solve_three():
+        assert solve(['eat', 'ate', 'zed']) == [['eat', 'ate'], ['zed']]
+
+        def test_solve_empty():
+            assert solve([]) == []
