473c9e530915a8b155bfa544b6737565a89d48fa
Uldis Sturms <uldis.sturms@gmail.com>
2020-04-08 09:57:54 +0100

    feat(leetcode-876): middle of the linked list

---
+++ leetcode/876_middle_of_the_linked_list_pytest.py
@@ -0,0 +1,63 @@
+# [https://leetcode.com/problems/middle-of-the-linked-list]
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def solve(n):
+    # fast and slow pointers
+    '''
+    (1) 0 0 < if fast.next is none - return slow
+    (2) 1 2 if fast is none - return slow
+    (3) 1 2 -> 1 - return slow
+    (4) 2 4 -> 2 - return slow
+    '''
+
+    s = n
+    f = n
+
+    while (f and f.next):
+        s = s.next
+        f = f.next.next
+
+    return s
+
+
+def test_single():
+    head = ListNode('h')
+    assert solve(head) == head
+
+
+def test_odd():
+    n1 = ListNode(1)
+    n2 = ListNode(2)
+    n3 = ListNode(3)
+    n4 = ListNode(4)
+    n5 = ListNode(5)
+
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+
+    assert(solve(n1).val) == n3.val
+
+
+def test_even():
+    n1 = ListNode(1)
+    n2 = ListNode(2)
+    n3 = ListNode(3)
+    n4 = ListNode(4)
+    n5 = ListNode(5)
+    n6 = ListNode(6)
+
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    n5.next = n6
+
+    assert(solve(n1).val) == n4.val
