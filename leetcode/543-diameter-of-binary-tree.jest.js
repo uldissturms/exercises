b0128e246d0f1b554cf06a82cffff880d53929b0
Uldis Sturms <uldis.sturms@gmail.com>
2020-04-21 13:57:50 +0100

    chore(leetcode-543): refactor binary tree diameter solution

--- leetcode/543-diameter-of-binary-tree.jest.js
+++ leetcode/543-diameter-of-binary-tree.jest.js
@@ -1,39 +1,50 @@
 // [https://leetcode.com/problems/diameter-of-binary-tree]
 
 test('solve', () => {
-  expect(diameterOfBinaryTree({
-    val: 1,
-    left: {
-      val: 2,
+  expect(
+    solve({
+      val: 1,
       left: {
-        val: 4
+        val: 2,
+        left: {
+          val: 4
+        },
+        right: {
+          val: 5
+        }
       },
       right: {
-        val: 5,
-      },
-    },
-    right: {
-      val: 3
-    }
-  })).toEqual(3)
+        val: 3
+      }
+    })
+  ).toEqual(3)
+  expect(solve(undefined)).toEqual(0)
+  expect(solve({ val: 1 })).toEqual(0)
+  expect(
+    solve({
+      val: 1,
+      left: {
+        val: 2
+      }
+    })
+  ).toEqual(1)
 })
 
-const diameterOfBinaryTree = (root) => {
-  const [b] = solve(root)
-  return Math.max(0, b - 1)
-};
+const solve = n => {
+  const dfs = n => {
+    if (n == null) {
+      return [0, 0]
+    }
 
-const solve = (n) => {
-  if (n == null) {
-    return [0, 0]
+    const [lm, lh] = dfs(n.left)
+    const [rm, rh] = dfs(n.right)
+
+    const h = Math.max(lh, rh) + 1
+    const m = Math.max(lh + rh + 1, lm, rm, 1)
+
+    return [m, h]
   }
 
-  const [lm, lh] = solve(n.left)
-  const [rm, rh] = solve(n.right)
-
-  const h = Math.max(lh, rh) + 1
-  const m = Math.max((lh + rh + 1), lm, rm, 1)
-
-  return [m, h]
+  const [b] = dfs(n)
+  return Math.max(0, b - 1)
 }
-
