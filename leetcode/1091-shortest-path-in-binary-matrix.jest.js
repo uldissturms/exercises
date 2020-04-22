45d4fb0ad0a3ee61128d7d0349f527431ea07664
Uldis Sturms <uldis.sturms@gmail.com>
2020-04-22 10:25:11 +0100

    chore(leetcode-1091): refactor shortest path in binary matrix a little bit

--- leetcode/1091-shortest-path-in-binary-matrix.jest.js
+++ leetcode/1091-shortest-path-in-binary-matrix.jest.js
@@ -8,66 +8,62 @@ test('solve', () => {
   expect(solve([[1, 0]])).toEqual(-1)
 
   expect(solve([[0, 1], [1, 0]])).toEqual(2)
-  expect(solve([[0,0,0],[1,1,0],[1,1,0]])).toEqual(4)
-  expect(solve([[0,0,0]])).toEqual(3)
+  expect(solve([[0, 0, 0], [1, 1, 0], [1, 1, 0]])).toEqual(4)
+  expect(solve([[0, 0, 0]])).toEqual(3)
 })
 
 const valid = 0
 
-const search = (grid, sr, sc, tr, tc) => {
-  if (grid.length === 0) {
+const keyFor = (x, y) => `${x}_${y}`
+
+// share an edge or corner
+const childrenFor = (x, y, xs, s) =>
+  [
+    [x + 1, y], // top
+    [x - 1, y], // bottom
+    [x, y - 1], // left
+    [x, y + 1], // right
+    [x + 1, y - 1], // top left corner
+    [x + 1, y + 1], // top right corner
+    [x - 1, y - 1], // bottom left corner
+    [x - 1, y + 1] // bottom right corner
+  ].filter(
+    ([x1, y1]) =>
+      x1 >= 0 &&
+      y1 >= 0 &&
+      x1 < xs.length &&
+      y1 < xs[x1].length &&
+      xs[x1][y1] === valid &&
+      !s.has(keyFor(x1, y1))
+  )
+
+const uniq = xs => {
+  const s = new Set()
+  const res = []
+
+  for (const [x, y] of xs) {
+    const k = keyFor(x, y)
+    if (!s.has(k)) {
+      res.push([x, y])
+      s.add(k)
+    }
+  }
+
+  return res
+}
+const search = (xs, sr, sc, tr, tc) => {
+  if (xs.length === 0) {
     return -1
   }
 
-  if (grid[sr][sc] !== valid || grid[tr][tc] !== valid) {
+  if (xs[sr][sc] !== valid || xs[tr][tc] !== valid) {
     return -1
   }
-
-  const gL = grid.length
-  const gW = grid[0].length
 
   let level = 1
   let q = [[sr, sc]]
   let c = []
   const s = new Set()
-
-  const keyFor = (x, y) => `${x}_${y}`
-
-  // share an edge or corner
-  const childrenFor = (x, y) =>
-    [
-      [x + 1, y], // top
-      [x - 1, y], // bottom
-      [x, y - 1], // left
-      [x, y + 1],  // right
-      [x + 1, y - 1], // top left corner
-      [x + 1, y + 1], // top right corner
-      [x - 1, y - 1], // bottom left corner
-      [x - 1, y + 1], // bottom right corner
-    ].filter(
-      ([x1, y1]) =>
-        x1 >= 0 &&
-        y1 >= 0 &&
-        x1 < gL &&
-        y1 < gW &&
-        !s.has(keyFor(x1, y1)) &&
-        grid[x1][y1] === valid
-    )
-
-  const uniq = xs => {
-    const s = new Set()
-    const res = []
-
-    for (const [x, y] of xs) {
-      const k = keyFor(x, y)
-      if (!s.has(k)) {
-        res.push([x, y])
-        s.add(k)
-      }
-    }
-
-    return res
-  }
 
   while (q.length > 0) {
     const [x, y] = q.shift()
@@ -78,14 +74,14 @@ const search = (grid, sr, sc, tr, tc) => {
     }
 
     if (!s.has(k)) {
-      const cs = childrenFor(x, y)
+      const cs = childrenFor(x, y, xs, s)
       c.push(...cs)
       s.add(k)
     }
 
     if (q.length === 0) {
       level++
-      q = uniq(c)
+      q = c
       c = []
     }
   }
