32655081e910bb4c977037d38a23463eb9332e1c
Uldis Sturms <uldis.sturms@gmail.com>
2020-04-29 07:21:12 +0100

    chore: refactor test a bit

--- leetcode/278-first-bad-version.jest.js
+++ leetcode/278-first-bad-version.jest.js
@@ -1,24 +1,12 @@
 // [https://leetcode.com/problems/first-bad-version]
 
-/**
- * Definition for isBadVersion()
- *
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
- */
+test('solve', () => {
+  const isBadVersion = x => x >= 4
+  expect(solution(isBadVersion)(20)).toEqual(4)
+  expect(solution(isBadVersion)(10)).toEqual(4)
+})
 
-/**
- * @param {function} isBadVersion()
- * @return {function}
- */
-var solution = function(isBadVersion) {
-  /**
-   * @param {integer} n Total versions
-   * @return {integer} The first bad version
-   */
+const solution = (isBadVersion) => {
   return function(n) {
     const bs = (s, e) => {
       if (s === e) {
@@ -38,21 +26,3 @@ var solution = function(isBadVersion) {
     return bs(1, n)
   }
 }
-
-/*
-
-b: 4
-
-s: 1, e: 6, m: 3 -> f
-s: 4, e: 6, m: 5 -> t
-s: 4, e: 5, m: 4 -> t
-
-b: 2
-s: 1, e: 2, m: 1 -> f
-s: 2, e: 2 -> 2
-
-b: 1
-s: 1, e: 2, m: 1 -> t
-s: 1, e: 1 -> 1
-
-*/
